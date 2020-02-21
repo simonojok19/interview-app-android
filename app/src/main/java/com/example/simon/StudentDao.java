9398ba0fafd96b5aafeda27fdb1c80b3b95cd6a9
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-21 11:43:28 +0300

    added dao for students

--- app/src/main/java/com/example/simon/StudentDao.java
+++ app/src/main/java/com/example/simon/StudentDao.java
@@ -1,4 +1,21 @@
 package com.example.simon;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface StudentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertStudent(Student student);
+
+    @Query("DELETE FROM student_table")
+    void deleteAllStudent();
+
+    @Query("SELECT * from student_table ORDER BY mId ASC")
+    List<Student> getAllStudent();
 }
